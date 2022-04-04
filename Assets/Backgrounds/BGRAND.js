88ed8a3cae8ebb33b2a0d689de5d13cb30da9fe6
DeverellManning <71154873+DeverellManning@users.noreply.github.com>
2022-04-04 11:44:39 -0600

    Update BGRAND.js

--- Assets/Backgrounds/BGRAND.js
+++ Assets/Backgrounds/BGRAND.js
@@ -2,7 +2,7 @@ function randBG(){
 
 var nm1=["pexels-aj-povey-216076.jpg", "pexels-markus-spiske-113338.jpg", "pexels-markus-spiske-138903.jpg", "pexels-pixabay-158223.jpg"];
 
-rnd=Math.floor(Math.random()*nm1.length);
+var rnd=Math.floor(Math.random()*nm1.length);
 
 var url=nm1[rnd];
 
