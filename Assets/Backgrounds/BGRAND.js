7ddc4a55bf28a385f1f7147c5ca3702e46789228
DeverellManning <71154873+DeverellManning@users.noreply.github.com>
2022-04-08 21:21:42 -0600

    Update BGRAND.js

--- Assets/Backgrounds/BGRAND.js
+++ Assets/Backgrounds/BGRAND.js
@@ -6,17 +6,17 @@ var rnd=Math.floor(Math.random()*nm1.length);
 
 var url=nm1[rnd];
   
-alert(url);
+//alert(url);
   
   var t1="url('https://deverellmanning.github.io/Assets/Backgrounds/";
   var t2="')";
   var t3=t1.concat(url);
   var t4=t3.concat(t2);
-  alert(t4);
+  //alert(t4);
 
   document.body.style.backgroundImage = t4; 
   
-    alert(document.body.style.backgroundImage);
+    //alert(document.body.style.backgroundImage);
   
   
 var element=document.createElement("div");
